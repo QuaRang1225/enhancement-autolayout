a4765081d76dfc5ab82342a0d8018e21d5443b7c
QuaRang1225 <dbduddnd1225@gmail.com>
2024-01-10 12:07:46 +0900

    라벨추가
    - 스토리보드와 코드 연결 구현
    - 코드와 스토리 보드를 병합하여 코드 구현

--- EX_UIkit/ViewControllers/ViewController.swift
+++ EX_UIkit/ViewControllers/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
         return label
     }()
     
+    @IBOutlet weak var helloLabel: UILabel!
+    
     //뷰가 생성되었을때
     override func viewDidLoad() {
         
@@ -25,8 +27,8 @@ class ViewController: UIViewController {
         
         view.backgroundColor = .blue
         view.addSubview(titleLabel)
+        view.addSubview(helloLabel)
         // Do any additional setup after loading the view.
-        
         titleLabel.translatesAutoresizingMaskIntoConstraints = false    //위치고정 false
         titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true    //가로세로 중앙에 맞춤
         titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
