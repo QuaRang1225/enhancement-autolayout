16f59b703a30ff5918c2304e9f4853059afbc207
QuaRang1225 <dbduddnd1225@gmail.com>
2024-01-12 18:09:06 +0900

    오토레이아웃 다이나믹 테이블 뷰 생성
    - 스토리 보드 및 테이블 셀 뷰 추가
    - 오토레이아웃 설정

---
+++ EX_UIkit/ViewControllers/DynamicTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DynamicTableViewController.swift
+//  EX_UIkit
+//
+//  Created by 유영웅 on 1/12/24.
+//
+
+import Foundation
+import UIKit
+
+class DynamicTableViewController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
