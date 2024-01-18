ecf2b99f12972ca25655eb7c9184a6208d08aa60
QuaRang1225 <dbduddnd1225@gmail.com>
2024-01-18 15:31:56 +0900

    스택뷰 예제 추가
    - 기존 전화거는 스택뷰에 한글자씩 지우는 부분 추가

--- EX_AutoLayout/ViewControllers/StackViewController.swift
+++ EX_AutoLayout/ViewControllers/StackViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class StackViewController:UIViewController{
     
     @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet var phoneNumberButtons: [CircleButton]!
     @IBOutlet weak var phoneCallButton: CircleButton!
+    
     
     var phoneNumberString = ""{
         didSet{
@@ -25,16 +27,27 @@ class StackViewController:UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         for bttnItem in phoneNumberButtons{
             bttnItem.addTarget(self, action: #selector (onNumberBttnClicked), for: .touchUpInside)
         }
+        deleteButton.addTarget(self, action: #selector(onDeleteBttnClicked), for: .touchUpInside)
         phoneCallButton.addTarget(self, action: #selector(onCallBttnClicked), for: .touchUpInside)
+        
     }
     @objc fileprivate func onNumberBttnClicked(sender:UIButton){
-        guard let inputString = sender.titleLabel?.text else {return}
-        phoneNumberString.append(inputString)
+        if sender.imageView?.image == UIImage(systemName: "staroflife.fill"){
+            phoneNumberString.append("*")
+        }else{
+            guard let inputString = sender.titleLabel?.text else {return}
+            phoneNumberString.append(inputString)
+        }
     }
     @objc fileprivate func onCallBttnClicked(sender:UIButton){
         phoneNumberString.removeAll()
     }
+    @objc fileprivate func onDeleteBttnClicked(sender:UIButton){
+        phoneNumberString.removeLast()
+    }
+    
 }
